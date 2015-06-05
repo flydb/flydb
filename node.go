bcf6b87fa8adad433da5bf0a95694a609424a03d
dong <ddliuhb@gmail.com>
2015-06-05 18:46:09 +0800

    :cloud:

--- node.go
+++ node.go
@@ -4,14 +4,33 @@ type Node {
 
 }
 
-func (this *Node) Get(key string) *Node {
+// Get child node by path
+func (this *Node) Get(path string) *Node {
 
 }
 
+// Check existence of child node
 func (this *Node) Has(key string) *Node bool {
 
 }
 
+// Set value of child node by path
 func (this *Node) Set(key string, v interface{}) error {
+
+}
+
+func (this *Node) ToJSON() []byte {
+
+}
+
+func (this *Node) LoadJSON(v []byte) {
+
+}
+
+func (this *Node) String() string {
+
+}
+
+func (this *Node) Int() int {
 
 }
